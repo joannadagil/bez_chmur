39ac9ead9fcda865174172907f599e65c2e164ad
joannadagil <asiadagil@gmail.com>
2026-03-30 14:51:07 +0200

    registered database models in backend

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Venue)
+admin.site.register(Event)
+admin.site.register(EventInstance)
+admin.site.register(SeatCategory)
+admin.site.register(Seat)
+admin.site.register(EventSeat)
+admin.site.register(Order)
+admin.site.register(OrderSeat)
+admin.site.register(Payment)
